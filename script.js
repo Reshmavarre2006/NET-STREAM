db9032e653b7d7041c8919cc37205b938a5c872a
Reshmavarre2006 <varrereshma@gmail.com>
2026-02-01 22:51:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function scrollLeft(id) {
+    document.getElementById(id).scrollLeft -= 300;
+}
+
+function scrollRight(id) {
+    document.getElementById(id).scrollLeft += 300;
+}
+// Open YouTube video when movie poster is clicked
+function openVideo(url) {
+    window.open(url, "_blank");
+}
+
+
+
+
+
+
+
+
+
+
+
+
